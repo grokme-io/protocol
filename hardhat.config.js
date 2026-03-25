1341777d8a5aa3cd5873cabe17000809d288a441
grokme-io <mail@grokme.io>
2026-03-25 15:15:37 +0100

    fix: add Solidity 0.8.28 compiler for V2 contracts
    Dual compiler: 0.8.28 (ArenaV2 + Trophy) + 0.8.20 (GrokNFT + V1 Arena)

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,14 +3,16 @@ require("@nomicfoundation/hardhat-chai-matchers");
 
 module.exports = {
   solidity: {
-    version: "0.8.20",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
+    compilers: [
+      {
+        version: "0.8.28",
+        settings: { optimizer: { enabled: true, runs: 200 }, viaIR: true },
       },
-      viaIR: true
-    }
+      {
+        version: "0.8.20",
+        settings: { optimizer: { enabled: true, runs: 200 }, viaIR: true },
+      },
+    ],
   },
   paths: {
     sources: "./contracts",
